d5084b4b0dfce3f7728417a187bb2ef285fbcd14
Takahiro Hara <takahiro.hb@gmail.com>
2023-01-15 01:19:03 +0900

    サーバーを起動する処理を追加、およびv1/healtcheckエンドポイントを追加

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,5 +1,47 @@
 package main
 
-func main() {
+import (
+	"flag"
+	"fmt"
+	"net/http"
+	"os"
+	"time"
+)
 
+type config struct {
+	port int
+	env  string
+}
+
+type application struct {
+	config config
+}
+
+func main() {
+	var cfg config
+
+	flag.IntVar(&cfg.port, "port", 8000, "API server port")
+	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
+	flag.Parse()
+
+	app := &application{
+		config: cfg,
+	}
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/v1/healthcheck", app.healthcheckHandler)
+
+	srv := &http.Server{
+		Addr:         fmt.Sprintf(":%d", cfg.port),
+		Handler:      mux,
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 30 * time.Second,
+	}
+
+	err := srv.ListenAndServe()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
