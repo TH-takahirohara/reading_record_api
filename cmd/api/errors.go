327c9854492e0dbb08945182a7f87e2ff99f4207
Takahiro Hara <takahiro.hb@gmail.com>
2023-01-18 23:11:22 +0900

    エラーのロギング処理における誤りを修正

--- cmd/api/errors.go
+++ cmd/api/errors.go
@@ -10,7 +10,7 @@ func (app *application) logError(r *http.Request, err error) {
 	app.logger.WithFields(logrus.Fields{
 		"request_method": r.Method,
 		"request_url":    r.URL.String(),
-	})
+	}).Error(err)
 }
 
 func (app *application) errorResponse(w http.ResponseWriter, r *http.Request, status int, message any) {
