01b68cb40215472e2ce008b4e900906ef494d935
Takahiro Hara <takahiro.hb@gmail.com>
2023-01-18 23:03:48 +0900

    エラーレスポンスに関するメソッドを追加

---
+++ cmd/api/errors.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/sirupsen/logrus"
+)
+
+func (app *application) logError(r *http.Request, err error) {
+	app.logger.WithFields(logrus.Fields{
+		"request_method": r.Method,
+		"request_url":    r.URL.String(),
+	})
+}
+
+func (app *application) errorResponse(w http.ResponseWriter, r *http.Request, status int, message any) {
+	env := envelope{"error": message}
+
+	err := app.writeJSON(w, status, env, nil)
+	if err != nil {
+		app.logError(r, err)
+		w.WriteHeader(500)
+	}
+}
+
+func (app *application) serverErrorResponse(w http.ResponseWriter, r *http.Request, err error) {
+	app.logError(r, err)
+
+	message := "サーバー側での処理において問題が発生しました。"
+	app.errorResponse(w, r, http.StatusInternalServerError, message)
+}
