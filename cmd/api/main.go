d567df2c267caa0fff024ebc850c9d3594a05290
Takahiro Hara <takahiro.hb@gmail.com>
2023-01-20 01:39:18 +0900

    DBとのコネクションプール確立処理の追加

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,6 +1,12 @@
 package main
 
 import (
+	"context"
+	"database/sql"
+	"fmt"
+	"time"
+
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/sirupsen/logrus"
 )
 
@@ -19,6 +25,14 @@ func main() {
 		logger.Fatal(err)
 	}
 
+	db, err := openDB(cfg)
+	if err != nil {
+		logger.Fatal(err)
+	}
+	defer db.Close()
+
+	logger.Info("database connection pool established")
+
 	app := &application{
 		config: cfg,
 		logger: logger,
@@ -28,4 +42,34 @@ func main() {
 	if err != nil {
 		logger.Fatal(err)
 	}
+}
+
+func openDB(cfg *Config) (*sql.DB, error) {
+	db, err := sql.Open("mysql", fmt.Sprintf(
+		"%s:%s@tcp(%s:%d)/%s?parseTime=true",
+		cfg.DBUser, cfg.DBPassword, cfg.DBHost, cfg.DBPort, cfg.DBName,
+	))
+	if err != nil {
+		return nil, err
+	}
+
+	db.SetMaxOpenConns(cfg.DBMaxOpenConns)
+	db.SetMaxIdleConns(cfg.DBMaxIdleConns)
+
+	duration, err := time.ParseDuration(cfg.DBMaxIdleTime)
+	if err != nil {
+		return nil, err
+	}
+
+	db.SetConnMaxIdleTime(duration)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	err = db.PingContext(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
 }
