76761a41570cbf36e1bf4e1ed604ace191e9705d
Takahiro Hara <takahiro.hb@gmail.com>
2023-01-22 17:03:39 +0900

    ユーザー登録時のアクティベーショントークン作成処理を追加

--- cmd/api/users.go
+++ cmd/api/users.go
@@ -3,6 +3,7 @@ package main
 import (
 	"errors"
 	"net/http"
+	"time"
 
 	"github.com/TH-takahirohara/reading_record_api/internal/data"
 	"github.com/TH-takahirohara/reading_record_api/internal/validator"
@@ -53,8 +54,18 @@ func (app *application) registerUserHandler(w http.ResponseWriter, r *http.Reque
 		return
 	}
 
+	token, err := app.models.Tokens.New(user.ID, 24*time.Hour, data.ScopeActivation)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+
 	app.background(func() {
-		err = app.mailer.Send(user.Email, "user_welcome.tmpl", user)
+		data := map[string]any{
+			"activationToken": token.Plaintext,
+		}
+
+		err = app.mailer.Send(user.Email, "user_welcome.tmpl", data)
 		if err != nil {
 			app.logger.Error(err)
 		}
