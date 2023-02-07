a10836055cb77b77a2c45a2ccd80f1d1c9bbb9d5
Takahiro Hara <takahiro.hb@gmail.com>
2023-02-07 22:55:26 +0900

    エラーメッセージのレスポンスの構造を変更

--- cmd/api/errors.go
+++ cmd/api/errors.go
@@ -28,36 +28,36 @@ func (app *application) serverErrorResponse(w http.ResponseWriter, r *http.Reque
 	app.logError(r, err)
 
 	message := "サーバー側の処理において問題が発生しました。"
-	app.errorResponse(w, r, http.StatusInternalServerError, message)
+	app.errorResponse(w, r, http.StatusInternalServerError, envelope{"msg": message})
 }
 
 func (app *application) badRequestResponse(w http.ResponseWriter, r *http.Request, err error) {
-	app.errorResponse(w, r, http.StatusBadRequest, err.Error())
+	app.errorResponse(w, r, http.StatusBadRequest, envelope{"msg": err.Error()})
 }
 
 func (app *application) authenticationRequiredResponse(w http.ResponseWriter, r *http.Request) {
 	message := "このリソースにアクセスするためには認証が必要です"
-	app.errorResponse(w, r, http.StatusUnauthorized, message)
+	app.errorResponse(w, r, http.StatusUnauthorized, envelope{"msg": message})
 }
 
 func (app *application) inactiveAccountResponse(w http.ResponseWriter, r *http.Request) {
 	message := "このリソースにアクセスするためにはユーザーアカウントの有効化が必要です"
-	app.errorResponse(w, r, http.StatusForbidden, message)
+	app.errorResponse(w, r, http.StatusForbidden, envelope{"msg": message})
 }
 
 func (app *application) notPermittedResponse(w http.ResponseWriter, r *http.Request) {
 	message := "このリソースにアクセスするために必要な権限がありません"
-	app.errorResponse(w, r, http.StatusForbidden, message)
+	app.errorResponse(w, r, http.StatusForbidden, envelope{"msg": message})
 }
 
 func (app *application) notFoundResponse(w http.ResponseWriter, r *http.Request) {
 	message := "要求されたリソースが見つかりませんでした"
-	app.errorResponse(w, r, http.StatusNotFound, message)
+	app.errorResponse(w, r, http.StatusNotFound, envelope{"msg": message})
 }
 
 func (app *application) methodNotAllowedResponse(w http.ResponseWriter, r *http.Request) {
 	message := fmt.Sprintf("%s メソッドはこのリソースでサポートされていません", r.Method)
-	app.errorResponse(w, r, http.StatusMethodNotAllowed, message)
+	app.errorResponse(w, r, http.StatusMethodNotAllowed, envelope{"msg": message})
 }
 
 func (app *application) failedValidationResponse(w http.ResponseWriter, r *http.Request, errors map[string]string) {
@@ -66,17 +66,17 @@ func (app *application) failedValidationResponse(w http.ResponseWriter, r *http.
 
 func (app *application) editConflictResponse(w http.ResponseWriter, r *http.Request) {
 	message := "更新処理の競合が発生したため、更新に失敗しました。再度リクエストを実行してください"
-	app.errorResponse(w, r, http.StatusConflict, message)
+	app.errorResponse(w, r, http.StatusConflict, envelope{"msg": message})
 }
 
 func (app *application) invalidCredentialsResponse(w http.ResponseWriter, r *http.Request) {
 	message := "無効な認証情報です"
-	app.errorResponse(w, r, http.StatusUnauthorized, message)
+	app.errorResponse(w, r, http.StatusUnauthorized, envelope{"msg": message})
 }
 
 func (app *application) invalidAuthenticationTokenResponse(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("WWW-Authenticate", "Bearer")
 
 	message := "認証トークンが含まれていないか、無効な値です"
-	app.errorResponse(w, r, http.StatusUnauthorized, message)
+	app.errorResponse(w, r, http.StatusUnauthorized, envelope{"msg": message})
 }
