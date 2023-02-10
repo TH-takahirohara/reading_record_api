f91c9aaa22bdf5709416bcecd74744ca5ce82274
Takahiro Hara <takahiro.hb@gmail.com>
2023-02-10 22:41:42 +0900

    一覧表示件数に関するJSON返却時のキー名をキャメルケースに変更

--- internal/data/filters.go
+++ internal/data/filters.go
@@ -14,8 +14,8 @@ type Filters struct {
 func ValidateFilters(v *validator.Validator, f Filters) {
 	v.Check(f.Page > 0, "page", "0より大きい値を指定してください")
 	v.Check(f.Page <= 10000, "page", "10000以下の値を指定してください")
-	v.Check(f.PageSize > 0, "page_size", "0より大きい値を指定してください")
-	v.Check(f.PageSize <= 100, "page_size", "100以下の値を指定してください")
+	v.Check(f.PageSize > 0, "pageSize", "0より大きい値を指定してください")
+	v.Check(f.PageSize <= 100, "pageSize", "100以下の値を指定してください")
 }
 
 func (f Filters) limit() int {
